2f17f7fd8e7654ef65fba264457c64340c049a7f
tianchi <tianchi_liu@yeah.net>
2017-05-30 21:01:39 +0800

    change readme

--- README.md
+++ README.md
@@ -8,18 +8,26 @@
 
 ## 说明 
 
-Welone 是一个简单的微博客户端，只提供浏览所关注的内容。该应用采用react-native架构实现，提供优美良的设计风格和友好的交互感受。   
+Welone 是一个简单的微博客户端，只提供浏览所关注的内容。用户可以以此为基础，开发自己喜欢的微博客户端。该应用采用react-native架构实现，提供优美良的设计风格和友好的交互感受。 
+
+因为该应用无法通过微博开发平台审核，所以直接下载安装该应用apk无法正常使用。请自行申请微博开发者账号，修改源代码中`android/app/src/main/java/com/welone/weibo/Constants.java`下的
+
+```java
+    /**
+     * 修改为自己的 APP_KEY
+     */
+    public static final String APP_KEY = "2029710884";
+```
+
+同时，在微博开发者平台中将自己的微博账号添加为测试账号。
+
+## 开发教程
+
+参考[博客](http://itanch.github.io/)。
 
 ## 内容   
 
-### 用户界面  
-![](http://7xky03.com1.z0.glb.clouddn.com/welone_3.png)
-
-### 主界面
-![](http://7xky03.com1.z0.glb.clouddn.com/welone_1.png)
-
-### 内容   
-![](http://7xky03.com1.z0.glb.clouddn.com/welone_2.png)
+![](http://7xky03.com1.z0.glb.clouddn.com/welone_gif_2.gif?imageView2/0/h/300)
 
 未完待续...
 
--- app/comp/WeiboToMeCard.android.js
+++ app/comp/WeiboToMeCard.android.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Image, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Card, CardItem, Thumbnail, Button, Text as TextBase, Icon } from 'native-base';
+import WeiboImage from './WeiboImage';
+import WeiboContent from './WeiboContent';
+
+import {NEW_COMMENT} from './WeiboConstant';
 
 export default class WeiboToMeCard extends Component {
 
+    //start the weibo content
+    startWeiboContent(data) {
+        const { navigator } = this.props;
+        if (navigator) {
+            navigator.push({
+                component: WeiboContent,
+                params: {
+                    data: data
+                }
+            })
+        }
+    }
 
     formatTime(time) {
         let times = time.split(' ');
@@ -16,7 +33,88 @@ export default class WeiboToMeCard extends Component {
         }
     }
 
+    getComments() {
+        if (this.props.getComments) {
+            this.props.getComments(NEW_COMMENT);
+        }
+    }
+
     render() {
-        return <Text>{this.props.data.text}</Text>;
+
+        let lc = (
+            <View style={styles.cardStyle}>
+                <View style={styles.cardHeadStyle}>
+                    <Thumbnail size={40} source={{ uri: this.props.weiData.user.profile_image_url }} />
+                    <View style={{ marginLeft: 7, justifyContent: 'space-around' }}>
+                        <Text style={{ fontSize: 13, color: '#eb984e' }}>{this.props.weiData.user.name}</Text>
+                        <Text style={{ fontSize: 11, color: '#b3b6b7' }}>{this.formatTime(this.props.weiData.created_at)}</Text>
+                    </View>
+                </View>
+
+                <View style={styles.cardBodyStyle} >
+                    <TextBase onPress={() => this.startWeiboContent(this.props.weiData)}>
+                        {this.props.weiData.text}
+                    </TextBase>
+                    <WeiboImage picUrls={this.props.weiData.pic_urls} />
+                </View>
+
+                <View style={styles.cardFootStyle}>
+                    <View style={styles.commentButton} >
+                        <Icon style={{ color: 'silver', fontSize: 20 }} name='ios-share-outline' />
+                        <Text style={{ color: 'silver', marginLeft: 3 }}>{this.props.weiData.reposts_count}</Text>
+                    </View>
+                    <TouchableOpacity style={styles.commentButton} onPress={() => this.getComments()}>
+                        <Icon style={{ color: 'silver', fontSize: 20 }} name='ios-chatboxes-outline' />
+                        <Text style={{ color: 'silver', marginLeft: 3 }}>{this.props.weiData.comments_count}</Text>
+                    </TouchableOpacity>
+                    <View style={styles.favourButton} >
+                        <Icon style={{ color: 'silver', fontSize: 20 }} name='ios-thumbs-up-outline' />
+                        <Text style={{ color: 'silver', marginLeft: 3 }}>{this.props.weiData.attitudes_count}</Text>
+                    </View>
+                </View>
+            </View>
+        );
+        return lc;
     }
 }
+
+const styles = StyleSheet.create({
+    cardStyle: {
+        marginTop: 7,
+        borderWidth: 1,
+        borderColor: '#e5e8e8',
+        borderRadius: 2,
+        backgroundColor: '#ffffff'
+    },
+    cardHeadStyle: {
+        flexDirection: 'row',
+        padding: 7,
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#e5e8e8'
+    },
+    cardBodyStyle: {
+        paddingHorizontal: 10,
+        paddingVertical: 7,
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#e5e8e8'
+    },
+    cardFootStyle: {
+        paddingVertical: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    commentButton: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRightWidth: 0.5,
+        borderRightColor: '#e5e8e8'
+    },
+    favourButton: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
